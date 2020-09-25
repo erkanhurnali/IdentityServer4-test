d852154764885ddec2bb8d1118a063a49abc6278
Erkan Hürnalı <56647597+erkanhurnali@users.noreply.github.com>
2020-09-25 19:12:43 +0300

    .

--- KimlikSunucusu_Identity/Startup.cs
+++ KimlikSunucusu_Identity/Startup.cs
@@ -57,18 +57,6 @@ namespace KimlikSunucusu
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
 
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
-
             services.AddCors(options =>
             {
                 // this defines a CORS policy called "default"
